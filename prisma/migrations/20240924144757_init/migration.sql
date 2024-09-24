5585fe06bd8cdd75fb64501560660ad99ecae024
Belicio Batista Cardoso <belloinfo@gmail.com>
2024-09-24 12:22:12 -0300

    Prisma init done

---
+++ prisma/migrations/20240924144757_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "report" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nomeCliente" TEXT NOT NULL,
+    "idSite" TEXT NOT NULL,
+    "altura" TEXT,
+    "endereco" TEXT NOT NULL,
+    "bairro" TEXT NOT NULL,
+    "numero" TEXT NOT NULL,
+    "cidade" TEXT NOT NULL,
+    "uf" TEXT NOT NULL,
+    "tecnico" TEXT NOT NULL,
+    "dataServico" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "finishedAt" DATETIME,
+    "tipoSite" TEXT NOT NULL,
+    "tipoEstrutura" TEXT NOT NULL
+);
