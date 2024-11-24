ce42daf6d1cc1aa7bbea80161ae334ce6b929daf
Belicio Cardoso <belloinfo@gmail.com>
2024-11-24 13:39:59 -0300

    Created page reportViewer

---
+++ app/reportviewer/[id]/page.tsx
@@ -0,0 +1,36 @@
+import {
+  getDescriptionsId,
+  getPhotoAnalisysById,
+  getRelatorioById,
+} from '@/app/(dashboard)/relatorio/actions'
+import { PhotoAnalisysType, Relatorio } from '@/lib/types'
+import HeaderReport from '../_components/headerReport'
+import PhotoAnalisys from '../_components/photoAnalisys'
+
+export default async function reportViewer({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const id = parseInt(params.id)
+  const photoAnalisys: PhotoAnalisysType[] = await getPhotoAnalisysById(id)
+  const relatorioHeader: Relatorio = await getRelatorioById(id)
+  const descriptions = await getDescriptionsId(id)
+
+  return (
+    <main>
+      <div className="flex w-full flex-col items-center bg-slate-300">
+        <HeaderReport
+          relatorioHeader={relatorioHeader}
+          descriptions={descriptions}
+          photoAnalisys={photoAnalisys}
+          id={id}
+        />
+        <aside className="container flex w-[780px] flex-col items-center py-4">
+          {<PhotoAnalisys photoAnalisys={photoAnalisys} />}
+        </aside>
+        <footer className="flex flex-col bg-white"></footer>
+      </div>
+    </main>
+  )
+}
---
+++ app/reportviewer/_components/photoAnalisys.tsx
@@ -0,0 +1,56 @@
+import imagem from '@/assets/image.svg'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
+import { PhotoAnalisysType } from '@/lib/types'
+import Image from 'next/image'
+
+export default function PhotoAnalisys({
+  photoAnalisys,
+}: {
+  photoAnalisys: PhotoAnalisysType[]
+}) {
+  return (
+    <>
+      {photoAnalisys.length > 0 ? (
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
+          {photoAnalisys.map((item, index) => (
+            <Card key={item.id}>
+              <div className="flex h-full flex-col items-center justify-between truncate">
+                <CardContent className="w-[430px] pt-2">
+                  <Image src={item.url} alt="Imagem" width={400} height={400} />
+                </CardContent>
+                <CardFooter className="mb-2 grid w-full grid-flow-col p-0">
+                  <div className="col-span-1 ml-2 border-2 py-1 text-center font-bold">
+                    <p>{index + 1}</p>
+                  </div>
+                  <div className="col-span-6 mr-2 flex h-9 items-center justify-center border-y-2 border-r-2 font-bold">
+                    <p>{item.description}</p>
+                  </div>
+                </CardFooter>
+              </div>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
+          {Array.from({ length: 3 }, (_, index) => (
+            <Card key={index}>
+              <div className="flex flex-col items-center truncate">
+                <CardContent className="w-[430px] pt-2">
+                  <Image src={imagem} alt="Imagem" width={400} height={400} />
+                </CardContent>
+                <CardFooter className="mb-2 grid w-full grid-flow-col p-0">
+                  <div className="col-span-1 ml-2 border-2 py-1 text-center font-bold">
+                    <p>{index + 1}</p>
+                  </div>
+                  <div className="col-span-6 mr-2 flex h-9 items-center justify-center border-y-2 border-r-2 font-bold">
+                    <p>Descrição da foto {index + 1}</p>
+                  </div>
+                </CardFooter>
+              </div>
+            </Card>
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
