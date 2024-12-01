be46974392e1981fbce3c79f6027692360829fc4
Belicio Cardoso <belloinfo@gmail.com>
2024-11-30 21:40:12 -0300

    refactor client storage

--- app/(dashboard)/relatorio/actions.ts
+++ app/(dashboard)/relatorio/actions.ts
@@ -1,5 +1,4 @@
 'use server'
-import { CONFIG } from '@/config'
 import prisma from '@/lib/prisma'
 import {
   DescriptionAnalisysType,
@@ -7,55 +6,15 @@ import {
   Relatorio,
   ReportCreateType,
   ReportRelType,
+  ReportUpdateType,
 } from '@/lib/types'
-import validateImageType from '@/lib/validateImageType'
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
-import { Client, Report } from '@prisma/client'
-
-const clientS3 = new S3Client({
-  region: CONFIG.providers.storage.region,
-  endpoint: CONFIG.providers.storage.endpoint,
-  credentials: {
-    accessKeyId: CONFIG.providers.storage.accessKeyId,
-    secretAccessKey: CONFIG.providers.storage.secretAccessKey,
-  },
-})
-
-// function validateImageType(file: File) {
-//   if (file.type.startsWith('image/')) return true
-//   console.log('Please select a valid image')
-//   return false
-// }
-
-// const exists = async (bucket: string) => {
-//   try {
-//     await clientS3.send(new HeadBucketCommand({ Bucket: bucket }))
-//     return true
-//   } catch (error) {
-//     return false
-//   }
-// }
+import { uploadObject } from '@/service/storage'
+import { Client } from '@prisma/client'
 
 export async function upLoadPhotoAnalisys(
   formData: FormData,
   idReport: number,
 ) {
-  // if (!(await exists(CONFIG.providers.storage.bucket))) {
-  //   console.log('Bucket does not exist')
-
-  //   const command = new CreateBucketCommand({
-  //     ACL: 'public-read',
-  //     Bucket: CONFIG.providers.storage.bucket,
-  //     CreateBucketConfiguration: {
-  //       LocationConstraint: 'EU',
-  //     },
-  //   })
-
-  //   const response = await clientS3.send(command)
-
-  //   console.log('Bucket created:', response)
-  // }
-
   const file = formData.get('file') as File
 
   const binaryFile = await file.arrayBuffer()
@@ -63,21 +22,9 @@ export async function upLoadPhotoAnalisys(
 
   const keyName = `reports/${idReport}/${file.name}`
 
-  // upload file to storage asynchronously
-  const uploadParams = {
-    Bucket: CONFIG.providers.storage.bucket,
-    Key: keyName,
-    Body: fileBuffer,
-    ContentType: file.type,
-  }
+  const result = uploadObject(keyName, fileBuffer, file)
 
-  if (validateImageType(file)) {
-    await clientS3.send(new PutObjectCommand(uploadParams))
-    const url = `${CONFIG.providers.storage.endpoint}/${CONFIG.providers.storage.bucket}/${keyName}`
-    return url
-  } else {
-    return null
-  }
+  return result
 }
 
 export async function savePhotoAnalisys(data: PhotoAnalisysType) {
@@ -204,7 +151,7 @@ export async function createReport(report: ReportCreateType) {
   }
 }
 
-export async function updateReport(data: Report) {
+export async function updateReport(data: ReportUpdateType) {
   return await prisma.report.update({
     where: {
       id: data.id,
