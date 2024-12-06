8ebc935acb2f492ee6864e13f83c484009b50a51
Belicio Batista Cardoso <belloinfo@gmail.com>
2024-12-06 09:35:38 -0300

    comentados

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,14 +6,7 @@ async function main() {
   const passwordHash = await hash('12345678', 10)
 
   // USERS
-  const users = [
-    { email: 'fulano.tal@gemail.com', name: 'Fulano Tal', role: Role.ADMIN },
-    { email: 'jane.doe@email.com', name: 'Jane Doe', role: Role.COORDINATOR },
-    { email: 'maria.doe@email.com', name: 'Maria Doe', role: Role.ANALYST },
-    { email: 'antonia.doe@email.com', name: 'Antonia Doe', role: Role.ANALYST },
-    { email: 'jose.doe@email.com', name: 'José Doe', role: Role.USER },
-    { email: 'pedro.doe@email.com', name: 'Pedro Doe', role: Role.TECHNICIAN },
-  ]
+  const users = [{ email: 'suporte@novacorrente.ind.br', name: 'Belicio Cardoso', role: Role.ADMIN }]
 
   for (const user of users) {
     await prisma.user.create({
@@ -30,25 +23,22 @@ async function main() {
 
   // Técnicos
 
-  const technicians = [{ name: 'Técnico 1' }, { name: 'Técnico 2' }]
+  // const technicians = [{ name: 'Técnico 1' }, { name: 'Técnico 2' }]
 
-  for (const technician of technicians) {
-    await prisma.technician.create({
-      data: technician,
-    })
-  }
+  // for (const technician of technicians) {
+  //   await prisma.technician.create({
+  //     data: technician,
+  //   })
+  // }
 
   // Clientes
-  const clients = [
-    { name: 'Cliente 1', img: 'https://via.placeholder.com/150' },
-    { name: 'Cliente 2' },
-  ]
+  // const clients = [{ name: 'Cliente 1' }, { name: 'Cliente 2' }]
 
-  for (const client of clients) {
-    await prisma.client.create({
-      data: client,
-    })
-  }
+  // for (const client of clients) {
+  //   await prisma.client.create({
+  //     data: client,
+  //   })
+  // }
 
   // Tipo de Estrutura
   const structureTypes = [
@@ -81,191 +71,191 @@ async function main() {
 
   // Sites
 
-  const client = await prisma.client.findFirst({
-    where: {
-      name: 'Cliente 1',
-    },
-  })
+  // const client = await prisma.client.findFirst({
+  //   where: {
+  //     name: 'Cliente 1',
+  //   },
+  // })
 
-  const structureType = await prisma.structureType.findFirst({
-    where: {
-      name: 'TORRE METÁLICA',
-    },
-  })
+  // const structureType = await prisma.structureType.findFirst({
+  //   where: {
+  //     name: 'TORRE METÁLICA',
+  //   },
+  // })
 
-  const siteType = await prisma.siteType.findFirst({
-    where: {
-      name: 'GREEN FIELD',
-    },
-  })
+  // const siteType = await prisma.siteType.findFirst({
+  //   where: {
+  //     name: 'GREEN FIELD',
+  //   },
+  // })
 
-  if (client && structureType && siteType) {
-    await prisma.site.create({
-      data: {
-        idSite: 'BR251552-AQW',
-        altura: '20 metros',
-        endereco: 'Rua das Flores',
-        numero: '123',
-        bairro: 'Jardim das Flores',
-        cidade: 'São Paulo',
-        uf: 'SP',
-        client: {
-          connect: {
-            id: client.id,
-          },
-        },
-        structureType: {
-          connect: {
-            id: structureType.id,
-          },
-        },
-        siteType: {
-          connect: {
-            id: siteType.id,
-          },
-        },
-      },
-    })
-  } else {
-    console.error('Client, Structure Type or Site Type not  found.')
-  }
+  // if (client && structureType && siteType) {
+  //   await prisma.site.create({
+  //     data: {
+  //       idSite: 'BR251552-AQW',
+  //       altura: '20 metros',
+  //       endereco: 'Rua das Flores',
+  //       numero: '123',
+  //       bairro: 'Jardim das Flores',
+  //       cidade: 'São Paulo',
+  //       uf: 'SP',
+  //       client: {
+  //         connect: {
+  //           id: client.id,
+  //         },
+  //       },
+  //       structureType: {
+  //         connect: {
+  //           id: structureType.id,
+  //         },
+  //       },
+  //       siteType: {
+  //         connect: {
+  //           id: siteType.id,
+  //         },
+  //       },
+  //     },
+  //   })
+  // } else {
+  //   console.error('Client, Structure Type or Site Type not  found.')
+  // }
 
-  const client2 = await prisma.client.findFirst({
-    where: {
-      name: 'Cliente 2',
-    },
-  })
+  // const client2 = await prisma.client.findFirst({
+  //   where: {
+  //     name: 'Cliente 2',
+  //   },
+  // })
 
-  const structureType2 = await prisma.structureType.findFirst({
-    where: {
-      name: 'SUPORTE',
-    },
-  })
+  // const structureType2 = await prisma.structureType.findFirst({
+  //   where: {
+  //     name: 'SUPORTE',
+  //   },
+  // })
 
-  const siteType2 = await prisma.siteType.findFirst({
-    where: {
-      name: 'ROOF TOP',
-    },
-  })
+  // const siteType2 = await prisma.siteType.findFirst({
+  //   where: {
+  //     name: 'ROOF TOP',
+  //   },
+  // })
 
-  if (client2 && structureType2 && siteType2) {
-    await prisma.site.create({
-      data: {
-        idSite: 'BR252556-ADF',
-        altura: '10 metros',
-        endereco: 'Rua das Rosas',
-        numero: '321',
-        bairro: 'Jardim das Rosas',
-        cidade: 'São Paulo',
-        uf: 'SP',
-        client: {
-          connect: {
-            id: client2.id,
-          },
-        },
-        structureType: {
-          connect: {
-            id: structureType2.id,
-          },
-        },
-        siteType: {
-          connect: {
-            id: siteType2.id,
-          },
-        },
-      },
-    })
-  } else {
-    console.error('Client, Structure Type or Site Type not  found.')
-  }
+  // if (client2 && structureType2 && siteType2) {
+  //   await prisma.site.create({
+  //     data: {
+  //       idSite: 'BR252556-ADF',
+  //       altura: '10 metros',
+  //       endereco: 'Rua das Rosas',
+  //       numero: '321',
+  //       bairro: 'Jardim das Rosas',
+  //       cidade: 'São Paulo',
+  //       uf: 'SP',
+  //       client: {
+  //         connect: {
+  //           id: client2.id,
+  //         },
+  //       },
+  //       structureType: {
+  //         connect: {
+  //           id: structureType2.id,
+  //         },
+  //       },
+  //       siteType: {
+  //         connect: {
+  //           id: siteType2.id,
+  //         },
+  //       },
+  //     },
+  //   })
+  // } else {
+  //   console.error('Client, Structure Type or Site Type not  found.')
+  // }
 
   // Relatórios
 
-  const user1 = await prisma.user.findFirst({
-    where: {
-      name: 'Fulano Tal',
-    },
-  })
+  // const user1 = await prisma.user.findFirst({
+  //   where: {
+  //     name: 'Fulano Tal',
+  //   },
+  // })
 
-  const user2 = await prisma.user.findFirst({
-    where: {
-      name: 'Jane Doe',
-    },
-  })
+  // const user2 = await prisma.user.findFirst({
+  //   where: {
+  //     name: 'Jane Doe',
+  //   },
+  // })
 
-  const site1 = await prisma.site.findFirst({
-    where: {
-      idSite: 'BR251552-AQW',
-    },
-  })
+  // const site1 = await prisma.site.findFirst({
+  //   where: {
+  //     idSite: 'BR251552-AQW',
+  //   },
+  // })
 
-  const site2 = await prisma.site.findFirst({
-    where: {
-      idSite: 'BR252556-ADF',
-    },
-  })
+  // const site2 = await prisma.site.findFirst({
+  //   where: {
+  //     idSite: 'BR252556-ADF',
+  //   },
+  // })
 
-  const technician1 = await prisma.technician.findFirst({
-    where: {
-      name: 'Técnico 1',
-    },
-  })
+  // const technician1 = await prisma.technician.findFirst({
+  //   where: {
+  //     name: 'Técnico 1',
+  //   },
+  // })
 
-  const technician2 = await prisma.technician.findFirst({
-    where: {
-      name: 'Técnico 2',
-    },
-  })
+  // const technician2 = await prisma.technician.findFirst({
+  //   where: {
+  //     name: 'Técnico 2',
+  //   },
+  // })
 
-  if (site1 && technician1 && user1) {
-    await prisma.report.create({
-      data: {
-        sites: {
-          connect: {
-            id: site1.id,
-          },
-        },
-        technician: {
-          connect: {
-            id: technician1.id,
-          },
-        },
-        user: {
-          connect: {
-            id: user1.id,
-          },
-        },
-        dateService: new Date('2024-10-10'),
-      },
-    })
-  } else {
-    console.error('Site, Technician, User, or Analyst not found.')
-  }
+  // if (site1 && technician1 && user1) {
+  //   await prisma.report.create({
+  //     data: {
+  //       sites: {
+  //         connect: {
+  //           id: site1.id,
+  //         },
+  //       },
+  //       technician: {
+  //         connect: {
+  //           id: technician1.id,
+  //         },
+  //       },
+  //       user: {
+  //         connect: {
+  //           id: user1.id,
+  //         },
+  //       },
+  //       dateService: new Date('2024-10-10'),
+  //     },
+  //   })
+  // } else {
+  //   console.error('Site, Technician, User, or Analyst not found.')
+  // }
 
-  if (site2 && technician2 && user2) {
-    await prisma.report.create({
-      data: {
-        sites: {
-          connect: {
-            id: site2.id,
-          },
-        },
-        technician: {
-          connect: {
-            id: technician2.id,
-          },
-        },
-        user: {
-          connect: {
-            id: user2.id,
-          },
-        },
-        dateService: new Date('2024-10-11'),
-      },
-    })
-  } else {
-    console.error('Site or Technician not found.')
-  }
+  // if (site2 && technician2 && user2) {
+  //   await prisma.report.create({
+  //     data: {
+  //       sites: {
+  //         connect: {
+  //           id: site2.id,
+  //         },
+  //       },
+  //       technician: {
+  //         connect: {
+  //           id: technician2.id,
+  //         },
+  //       },
+  //       user: {
+  //         connect: {
+  //           id: user2.id,
+  //         },
+  //       },
+  //       dateService: new Date('2024-10-11'),
+  //     },
+  //   })
+  // } else {
+  //   console.error('Site or Technician not found.')
+  // }
 
   console.log('Seeding completed successfully.')
 }
