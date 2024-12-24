45b5d751b190f9b069c4ae75a82ca8dff5d4d9d0
Belicio Cardoso <belloinfo@gmail.com>
2024-12-24 18:36:44 -0300

    continua

--- app/reportviewer/_components/photoAnalisys.tsx
+++ app/reportviewer/_components/photoAnalisys.tsx
@@ -14,25 +14,23 @@ export default function PhotoAnalisys({
       {photoAnalisys.length > 0 ? (
         <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
           {photoAnalisys.map((item, index) => (
-            <Card key={item.id}>
-              <div className="flex h-full flex-col items-center justify-between truncate">
-                <CardContent className="flex-grow flex items-center justify-center pt-0 pb-0 px-0">
-                  <Image src={item.url} alt="Imagem" width={350} height={350} />
-                </CardContent>
-                <CardFooter className="mb-2 grid max-h-20 w-full grid-flow-col p-2">
-                  <div className="col-span-1 flex h-full items-center justify-center border-2 py-1 font-bold">
-                    <p>{index + 1}</p>
-                  </div>
-                  <div className="col-span-4 mr-2 h-full border-2 p-2 font-bold">
-                    <div
-                      className="max-w-96 max-h-14 text-wrap"
-                      /* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */
-                      dangerouslySetInnerHTML={{ __html: item.description }}
-                    />
-                    {/* <p>{item.description}</p> */}
-                  </div>
-                </CardFooter>
-              </div>
+            <Card key={item.id} className="flex flex-col items-center justify-between truncate">
+              <CardContent className="w-[380px] h-[300px] flex items-center justify-center p-0">
+                <Image src={item.url} alt="Imagem" width={350} height={350} />
+              </CardContent>
+              <CardFooter className="mb-2 grid max-h-20 w-full grid-flow-col p-2">
+                <div className="col-span-1 flex h-full items-center justify-center border-2 py-1 font-bold">
+                  <p>{index + 1}</p>
+                </div>
+                <div className="col-span-4 mr-2 h-full border-2 p-2 font-bold">
+                  <div
+                    className="max-w-96 max-h-14 text-wrap"
+                    /* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */
+                    dangerouslySetInnerHTML={{ __html: item.description }}
+                  />
+                  {/* <p>{item.description}</p> */}
+                </div>
+              </CardFooter>
             </Card>
           ))}
         </div>
