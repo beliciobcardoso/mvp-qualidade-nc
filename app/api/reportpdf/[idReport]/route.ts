861a002fc4386d7688267d82381d010dc510d71c
Belicio Batista Cardoso <belloinfo@gmail.com>
2025-01-09 15:40:59 -0300

    ajuste para exibir mais imagens

--- app/api/reportpdf/[idReport]/route.ts
+++ app/api/reportpdf/[idReport]/route.ts
@@ -2,18 +2,27 @@ import puppeteer from 'puppeteer'
 
 export async function GET(request: Request, { params }: { params: { idReport: number } }) {
   console.log('Iniciando o puppeteer')
+
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   })
   console.log('Navegador iniciado.')
+
   const page = await browser.newPage()
   console.log('Nova página aberta.')
-  await page.setViewport({ width: 1600, height: 1024 })
-  await page.goto(`${process.env.NEXT_PUBLIC_URL_APP}/reportviewer/${params.idReport}`, {
+
+  await page.setViewport({ width: 1600, height: 4096 })
+  console.log('Tamanho da página definido.')
+
+  page.goto(`${process.env.NEXT_PUBLIC_URL_APP}/reportviewer/${params.idReport}`, {
     waitUntil: 'networkidle2',
   })
+
   console.log('Pagina carregada.')
+
   await page.emulateMediaType('print')
+  console.log('Tipo de midia definido.')
+
   const pdf = await page.pdf({
     format: 'LETTER',
     printBackground: true,
@@ -24,15 +33,15 @@ export async function GET(request: Request, { params }: { params: { idReport: nu
     //   </div>
     // </div>`,
     footerTemplate: `
-    <div style="font-size: 10px; padding-top: 5px; display: flex; justify-content: space-between; width: 100%; margin-left: 20px; margin-right: 20px;">
-      <div style="text-align: left;">
-        <span>${new Date().toLocaleDateString('pt-BR')}</span>
+      <div style="font-size: 10px; padding-top: 5px; display: flex; justify-content: space-between; width: 100%; margin-left: 20px; margin-right: 20px;">
+        <div style="text-align: left;">
+          <span>${new Date().toLocaleDateString('pt-BR')}</span>
+        </div>
+        <div>
+        <span>Pagina</span> - <span class="pageNumber"></span> / <span class="totalPages"></span>
+        </div>
       </div>
-      <div>
-      <span>Pagina</span> - <span class="pageNumber"></span> / <span class="totalPages"></span>
-      </div>
-    </div>
-  `,
+    `,
     margin: {
       top: '20px',
       right: '15px',
@@ -40,14 +49,15 @@ export async function GET(request: Request, { params }: { params: { idReport: nu
       left: '15px',
     },
   })
+  console.log('PDF gerado.')
 
   await browser.close()
+  console.log('Navegador fechado.')
 
   return new Response(pdf, {
+    status: 200,
     headers: {
       'Content-Type': 'application/pdf',
     },
   })
-
-  // return Response.json({ message: `Hello ${Number(params.idreport)}` })
 }
